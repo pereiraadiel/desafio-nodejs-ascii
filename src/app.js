30e1257b9829ca5846f474a44497b63087863330
Adiel Pereira <adielp16@gmail.com>
2021-02-02 17:19:38 -0300

    add create repository

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -16,6 +16,15 @@ app.get("/repositories", (request, response) => {
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const { title, url, techs} = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs
+  }
+  repositories.push(repository);
+  response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
